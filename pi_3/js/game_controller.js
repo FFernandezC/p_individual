f40244fdef2db05777fb10ccdaec746b9aef4351
Francisco <104916137+FFernandezC@users.noreply.github.com>
2023-03-23 16:56:58 +0100

    Update game_controller.js
    Solution to malfunction in starter game.

--- pi_3/js/game_controller.js
+++ pi_3/js/game_controller.js
@@ -20,6 +20,7 @@ var game = new Vue({
 		var json = localStorage.getItem("config") || '{"cards":2,"dificulty":"hard"}';
 		var options_data = JSON.parse(json);
 		this.num_cards=options_data.cards;
+		
 		//----------------------------------------------------------
 		
 		this.items.sort(function(){return Math.random() - 0.5}); // Array aleatòria
@@ -29,10 +30,10 @@ var game = new Vue({
 		
 		//Quan pasa 1 segon, les torna a posar a back
 			setTimeout(() => {
-				console.log("temps!")
 				for (var i = 0; i < this.items.length; i++){
 					Vue.set(this.current_card, i, {done: false, texture: back});
 				}
+				this.started_game = true;
 			}, 1000);
 		//----------------------------------------------------------
 		
@@ -47,33 +48,35 @@ var game = new Vue({
 		}
 	},
 	watch: {
-			current_card: function(value){
+		
+		current_card: function(value){
 				
 				//Surt de la funció
-				if(this.started_game===false)return;
-				
-				if (value.texture === back) return;
-				var front = null;
-				var i_front = -1;
-				for (var i = 0; i < this.current_card.length; i++){
-					if (!this.current_card[i].done && this.current_card[i].texture !== back){
-						if (front){
-							if (front.texture === this.current_card[i].texture){
-								front.done = this.current_card[i].done = true;
-								this.num_cards--;
-								console.log("Gira 1");
+				if(this.started_game === false){
+					return;
+				}
+				else{				
+					if (value.texture === back) return;
+					var front = null;
+					var i_front = -1;
+					for (var i = 0; i < this.current_card.length; i++){
+						if (!this.current_card[i].done && this.current_card[i].texture !== back){
+							if (front){
+								if (front.texture === this.current_card[i].texture){
+									front.done = this.current_card[i].done = true;
+									this.num_cards--;
+								}
+								else{
+									Vue.set(this.current_card, i, {done: false, texture: back});
+									Vue.set(this.current_card, i_front, {done: false, texture: back});
+									this.bad_clicks++;
+									break;
+								}
 							}
 							else{
-								Vue.set(this.current_card, i, {done: false, texture: back});
-								Vue.set(this.current_card, i_front, {done: false, texture: back});
-								this.bad_clicks++;
-								console.log("Gira 2");
-								break;
+								front = this.current_card[i];
+								i_front = i;
 							}
-						}
-						else{
-							front = this.current_card[i];
-							i_front = i;
 						}
 					}
 				}			
