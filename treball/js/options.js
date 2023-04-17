718265540e67b268987e8c472abdcabd9ee79b3d
Francisco <104916137+FFernandezC@users.noreply.github.com>
2023-04-17 17:19:25 +0200

    [use strict] en options.js

--- treball/js/options.js
+++ treball/js/options.js
@@ -1,10 +1,12 @@
+"use strict";
+
 var options = function(){
 	// Aquí dins hi ha la part privada de l'objecte
 	var options_data = {
-		cards:2, dificulty:"easy", mode_2: "easy"
+		cards:2, dificulty:"easy", mode_2: "mode_easy"
 	};
 	var load = function(){
-		var json = localStorage.getItem("config") || '{"cards":2,"dificulty":"hard"}';
+		var json = localStorage.getItem("config") || '{"cards":2,"dificulty":"easy","mode_2":"mode_easy"}';
 		options_data = JSON.parse(json);
 	};
 	var save = function(){
@@ -15,18 +17,17 @@ var options = function(){
 		el: "#options_id",
 		data: {
 			num: 2,
-			dificulty: "normal",
-			mode_2: "normal"
+			dificulty: "easy",
+			mode_2: "mode_easy"
 		},
 		created: function(){
 			this.num = options_data.cards;
 			this.dificulty = options_data.dificulty;
 			this.mode_2 = options_data.mode_2;
-			console.log(this.dificulty);
+			
 		},
 		watch: {
 			num: function(value){
-				console.log(value);
 				if (value < 2)
 					this.num = 2;
 				else if (value > 4)
@@ -36,14 +37,20 @@ var options = function(){
 		methods: { 
 			discard: function(){		//Acció quan premem "DISCARD"
 				this.num = options_data.cards;
+				console.log(this.num);
 				this.dificulty = options_data.dificulty;
+				console.log(this.dificulty);
 				this.mode_2 = options_data.mode_2;
+				console.log(this.mode_2);
 			},
 			save: function(){		//Acció quan premem "SAVE"
 				options_data.cards = this.num;
 				options_data.dificulty = this.dificulty;
 				options_data.mode_2 = this.mode_2;
 				save();
+				loadpage("../");
+			},
+			exit_menu: function(){
 				loadpage("../");
 			}
 		}
