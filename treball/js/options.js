3dea84a1b88452de050c88c1b37dc0e651ae2682
Francisco <104916137+FFernandezC@users.noreply.github.com>
2023-04-17 16:43:58 +0200

    canvis

--- treball/js/options.js
+++ treball/js/options.js
@@ -1,7 +1,7 @@
 var options = function(){
 	// Aquí dins hi ha la part privada de l'objecte
 	var options_data = {
-		cards:2, dificulty:"hard"
+		cards:2, dificulty:"easy", mode_2: "easy"
 	};
 	var load = function(){
 		var json = localStorage.getItem("config") || '{"cards":2,"dificulty":"hard"}';
@@ -15,14 +15,18 @@ var options = function(){
 		el: "#options_id",
 		data: {
 			num: 2,
-			dificulty: "normal"
+			dificulty: "normal",
+			mode_2: "normal"
 		},
 		created: function(){
 			this.num = options_data.cards;
 			this.dificulty = options_data.dificulty;
+			this.mode_2 = options_data.mode_2;
+			console.log(this.dificulty);
 		},
 		watch: {
 			num: function(value){
+				console.log(value);
 				if (value < 2)
 					this.num = 2;
 				else if (value > 4)
@@ -33,10 +37,12 @@ var options = function(){
 			discard: function(){
 				this.num = options_data.cards;
 				this.dificulty = options_data.dificulty;
+				this.mode_2 = options_data.mode_2;
 			},
 			save: function(){
 				options_data.cards = this.num;
 				options_data.dificulty = this.dificulty;
+				options_data.mode_2 = this.mode_2;
 				save();
 				loadpage("../");
 			}
@@ -55,11 +61,11 @@ var options = function(){
 		}
 	}; 
 }();
-
+/*
 console.log(options.getOptionsString());
 console.log(options.getNumOfCards());
 console.log(options.getDificulty());
-console.log(options.options_data);
+console.log(options.options_data);*/
 
 
 
