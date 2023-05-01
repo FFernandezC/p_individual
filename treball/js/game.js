e0cc555b99ec7bff46c5bf22d17765016a8edf21
Francisco <104916137+FFernandezC@users.noreply.github.com>
2023-05-01 15:38:29 +0200

    Show cards and back when time out

--- treball/js/game.js
+++ treball/js/game.js
@@ -7,6 +7,7 @@ class GameScene extends Phaser.Scene{
         this.firstClick = null;
         this.score = 100;
         this.correct = 0;
+        this.PlayEnded = false;
         this.name = prompt("Username") || "[ ]";
 		this.options_data = JSON.parse(localStorage.getItem("config") || '{"cards":2,"dificulty":"easy","mode_2":"mode_easy"}');
     }
@@ -21,24 +22,71 @@ class GameScene extends Phaser.Scene{
     }
     create () {
 
+        let points = 0, temps = 0;
+        if(this.options_data.dificulty == "easy"){points = 10; temps = 3000;}
+        else if(this.options_data.dificulty == "normal"){points = 20; temps = 2000;}
+        else{points = 30; temps = 1000;}
+
         /* Array of shuffled cards and background color */
         let arraycards = ['cb','co','sb','so','tb','to'];
-        let mixed_array = Phaser.Utils.Array.Shuffle(arraycards);
         //[I]: Inforation of [Phaser.Utils.Array.Shuffle] in: https://newdocs.phaser.io/docs/3.55.2/focus/Phaser.Utils.Array.Shuffle
-        console.log(mixed_array);
+        console.log("Array inicial: " + arraycards);
         this.cameras.main.setBackgroundColor(0xBFFCFF);
 
         this.add.text(25, 50, "Is playing: " + this.name, {fontFamily: 'Brush Script MT', fontSize: '25px', fill: '#000'});
         this.add.text(575, 50, "Score: " + this.score, {fontFamily: 'Brush Script MT', fontSize: '25px', fill: '#000'});
         //[I] Information of [this.add.text] in: https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Text.html
 
+        // Generate the Array of duplicate cards
+        let Generator_1 = 0, card_pos = 0;
+        let GeneratedArray = [];
+        while (Generator_1 < this.options_data.cards) {
+			GeneratedArray.push(arraycards[card_pos]);
+            GeneratedArray.push(arraycards[card_pos]);
+			card_pos++;
+			Generator_1++;
+		}
+        
+        console.log("Array duplidada: " + GeneratedArray);
+        // Shuffle the cards inside the Array
+        GeneratedArray = Phaser.Utils.Array.Shuffle(GeneratedArray);
+        console.log("Array definitiva Barejada: " + GeneratedArray);
 
-
+        // Show all cards on screen
+        let show_cards = 0, x = 150, linia = 0;
+        while (show_cards < this.options_data.cards*2) {
+            if (show_cards < 6){
+                this.add.image(x, 200, GeneratedArray[show_cards]);
+            }else if (show_cards == 6){
+                x = 150;
+                this.add.image(x, 350, GeneratedArray[show_cards]);
+            }else if (show_cards > 6){
+                this.add.image(x, 350, GeneratedArray[show_cards]);
+            }
+			show_cards++;
+			x+=100;
+		}
 
         // Cards Physics are static
-        this.cards_play = this.physics.add.staticGroup()
+        this.cards_play = this.physics.add.staticGroup();
 
-
+        // Show cards turned over
+        setTimeout(() => {
+            let loop_back = 0;
+            x = 150;
+            while (loop_back < this.options_data.cards*2) {
+                if (loop_back < 6){
+                    this.add.image(x, 200, 'back');
+                }else if (loop_back == 6){
+                    x = 150;
+                    this.add.image(x, 350, 'back');
+                }else if (loop_back > 6){
+                    this.add.image(x, 350, 'back');
+                }
+                loop_back++;
+                x+=100;
+            }
+        }, temps);
 
         // Gameplay of the game [clicks, loose, win and score]
         let i = 0;
@@ -47,32 +95,32 @@ class GameScene extends Phaser.Scene{
             i++;
             card.setInteractive();
             card.on('pointerup', ()=> {
-                card.disableBody(true, true);
-
-                if (this.firstClick){
-                    if (this.firstClick.card_id !== card.card_id){
-                        this.score -= 20;
-                        Textsc.setText('Score: ' + this.score);
-                        this.firstClick.enableBody(false, 0, 0, true, true);
-                        card.enableBody(false, 0, 0, true, true);
-                        if (this.score <= 0){
-                            alert("Game Over");
-                            Textsc.setText('Score: 0')
-                            loadpage("../")
+                if(this.PlayEnded != true){
+                    card.disableBody(true, true);
+                    if (this.firstClick){
+                        if (this.firstClick.card_id !== card.card_id){
+                            this.score -= points;
+                            Textsc.setText('Score: ' + this.score);
+                            this.firstClick.enableBody(false, 0, 0, true, true);
+                            card.enableBody(false, 0, 0, true, true);
+                            if (this.score <= 0){
+                                alert("Game Over");
+                                Textsc.setText('Score: 0')
+                                loadpage("../")
+                            }
                         }
-                    }
-                    else{
-                        this.correct++;
-                        if (this.correct >= 2){
-                            alert("You Win with " + this.score + " points.");
-                            loadpage("../")
+                        else{
+                            this.correct++;
+                            if (this.correct >= 2){
+                                alert("You Win with " + this.score + " points.");
+                                loadpage("../")
+                            }
                         }
+                        this.firstClick = null;
+                    }else{
+                        this.firstClick = card;
                     }
-                    this.firstClick = null;
-                }else{
-                    this.firstClick = card;
                 }
-
             }, card);
         });
     }
